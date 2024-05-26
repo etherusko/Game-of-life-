de61e04952f05b6ca8194fb8d97d5b6b7addc664
etherusko <etherusko.labs@gmail.com>
2024-05-26 04:50:10 -0600

    change initCells() and initNeighbors() to static Cell class methods

--- Cell.js
+++ Cell.js
@@ -1,5 +1,8 @@
 export class Cell{
     static color = 'yellow';
+    static rows = 150;
+    static cols = 240;
+    static cellsArray = [];
     constructor(x,y){
         this.state = Math.floor(Math.random()*2);
         this.pos = {
@@ -9,7 +12,7 @@ export class Cell{
         this.lifeAround = 0;
         this.neighbors = [];
     }
-    /*Méthods*/
+    /*Methods*/
     evalNeighbors(){
         this.neighbors.forEach(n => {if(n.state == 1) this.lifeAround++});
     }
@@ -20,5 +23,33 @@ export class Cell{
             if(this.lifeAround == 3) this.state = 1;
         }
         this.lifeAround = 0;
+    }
+    /*Static Methods*/  
+    static initCells(){
+        for(let i=0; i<this.rows; i++){
+            this.cellsArray[i]=[]
+            for(let j=0; j<this.cols; j++){
+                this.cellsArray[i].push(new Cell(j,i));
+            }
+        }
+        return this.cellsArray;
+    }
+    static initNeighbors(){
+        this.cellsArray.forEach(row => row.forEach(cell => {
+            let x = cell.pos.x;
+            let y = cell.pos.y;
+            if(x > 0){
+                cell.neighbors.push(this.cellsArray[y][x-1]);
+                if(y > 0)cell.neighbors.push(this.cellsArray[y-1][x-1]);
+                if(y < this.rows-1)cell.neighbors.push(this.cellsArray[y+1][x-1]);
+            }
+            if(x < this.cols-1){
+                cell.neighbors.push(this.cellsArray[y][x+1]);
+                if(y > 0)cell.neighbors.push(this.cellsArray[y-1][x+1]);
+                if(y < this.rows-1)cell.neighbors.push(this.cellsArray[y+1][x+1]);
+            }
+            if(y > 0)cell.neighbors.push(this.cellsArray[y-1][x])
+            if(y < this.rows-1)cell.neighbors.push(this.cellsArray[y+1][x])
+        }));
     }
 }
--- main.js
+++ main.js
@@ -2,61 +2,25 @@ import {Cell} from "./Cell.js";
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d',{alpha : false});
 const cellSize = 10;
-const rows = 50;
-const cols = 80;
-const cellsArray = [];
+const rows = Cell.rows;
+const cols = Cell.cols;
+const cellsArray = Cell.initCells();
 const cw = cellSize*cols;
 const ch = cellSize*rows;
 canvas.width = cw;
 canvas.height = ch;
-
 ctx.fillStyle = Cell.color;
-initCells();
-initNeighbors();
-console.log(cellsArray)
-
-function initCells(){
-    for(let i=0; i<rows; i++){
-        cellsArray[i]=[]
-        for(let j=0; j<cols; j++){
-            cellsArray[i].push(new Cell(j,i));
-        }
-    }
-}
-function initNeighbors(){
-    cellsArray.forEach(row => row.forEach(cell => {
-        let x = cell.pos.x;
-        let y = cell.pos.y;
-        if(x > 0){
-            cell.neighbors.push(cellsArray[y][x-1]);
-            if(y > 0)cell.neighbors.push(cellsArray[y-1][x-1]);
-            if(y < rows-1)cell.neighbors.push(cellsArray[y+1][x-1]);
-        }
-        if(x < cols-1){
-            cell.neighbors.push(cellsArray[y][x+1]);
-            if(y > 0)cell.neighbors.push(cellsArray[y-1][x+1]);
-            if(y < rows-1)cell.neighbors.push(cellsArray[y+1][x+1]);
-        }
-        if(y > 0)cell.neighbors.push(cellsArray[y-1][x])
-        if(y < rows-1)cell.neighbors.push(cellsArray[y+1][x])
-    }));
-}
-
+Cell.initNeighbors();
 requestAnimationFrame(loop)
 let frame = 0;
 function loop(){
     frame++;
-    if(frame%5==0){
-    ctx.clearRect(0,0,cw,ch);
-    //Evaluar vecinos
-    cellsArray.forEach(row => row.forEach(cell => cell.evalNeighbors()));
-    //update cells
-    cellsArray.forEach(row => row.forEach(cell => {
-        cell.updateCell();
-        if(cell.state == 1) ctx.fillRect(cell.pos.x*cellSize,cell.pos.y*cellSize,cellSize,cellSize);
-        cell.lifeAround=0;
-    }))
-    }
-    console.log('hola loop');
+    if(frame%4==0){
+        ctx.clearRect(0,0,cw,ch);
+        cellsArray.forEach(row => row.forEach(cell => cell.evalNeighbors()));
+        cellsArray.forEach(row => row.forEach(cell => {
+            cell.updateCell();
+            if(cell.state == 1) ctx.fillRect(cell.pos.x*cellSize,cell.pos.y*cellSize,cellSize,cellSize);
+    }))}
     requestAnimationFrame(loop)
 }
